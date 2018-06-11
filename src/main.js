32523045fef3b481121ff8608f80b6511a322625
chenhaoyun <chy405233403@outlook.com>
2018-06-11 15:24:16 +0800

    首页更改3

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ export default {
       /* eslint-disable */
       list: [{
         pagePath: 'pages/index/index',
-        text: '首页333',
+        text: '1首页',
         // iconPath: 'static/images/icon_nav_button.png',
         // selectedIconPath: 'static/images/icon_nav_button.png'
       },
